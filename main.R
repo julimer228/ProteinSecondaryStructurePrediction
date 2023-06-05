388db23aeacef9820b4c43b9bc8c88647d9371d3
julimer228 <julia-merta@wp.pl>
2023-06-05 22:17:11 +0200

    Added orthogonal encoding and window sampling

--- main.R
+++ main.R
@@ -1,13 +1,4 @@
----
-title: "The Protein Secondary Structure Prediction"
-output: html_notebook
----
 
-We have introduced a protein secondary structure prediction method based on the Logistic Regression classifier.
-
-First, we need to load the data into the workspace
-
-```{r}
 library(stringr)
 
 
@@ -20,9 +11,9 @@ orthogonally_encoding<-function(sequence){
                    "R", "S", "T", "V", "W", "Y","X");
   
   # initialize the matrix
-  encoded_sequence <- matrix(0, nrow = length(sequence), ncol = length(amino_acids))
+  encoded_sequence <- matrix(0, nrow = nchar(sequence), ncol = length(amino_acids))
   
-  for (i in 1:length(sequence)) {
+  for (i in 1:nchar(sequence)) {
     aa <- substring(sequence, i, i)
     idx <- match(aa, amino_acids)
     if (!is.na(idx)) {
@@ -67,19 +58,22 @@ create_window<-function(protein, index, window_size){
 
 sample_data<-function(protein, second_lv_struct, window_size){
   
-  samples<-list();
+  sample_list<-list();
   sample<-NULL; 
   
-  for(i in 1:length(protein)){
-    vector<-create_window(protein,i,window_size);
-   
-    sample$encoded_vector<-orthogonally_encoding(vector);
-    sample$struct<-substring(second_lv_struct,i,i);
+  for(i in 1:nchar(protein)){
     
-    samples<-append(sample, samples);
+    vector<-create_window(protein,i,window_size);
+    encoded_vector<-orthogonally_encoding(vector);
+    struct_str<-substring(second_lv_struct,i,i);
+    
+    sample$vector<-encoded_vector;
+    sample$struct<-struct_str;
+    
+    sample_list<-append(sample,sample_list);
   }
   
-  return(samples);
+  return(sample_list);
 }
 
 
@@ -101,20 +95,16 @@ read_protein_data<-function(filepath){
     } else if(i%%4 == 2){
       current$sequence<-line # read sequence
     } else if(i%%4 == 3){
-      current$structure<-line  #read secondary structure    
+      current$structure<-line;  #read secondary structure    
     } else if(i%%4 == 0){
-      current$sampled_data<-sample_data(current$sequence,current$structure,5)
+      current$sampled_data<-sample_data(current$sequence,current$structure,20)
       protein_data<-append(current, protein_data); # add protein to the list
     }
     
   }
-  return(protein_data)
+  return(protein_data);
 }
 
 protein_data = read_protein_data("test.txt");
-  
-  
-```
 
-```{}
-```
+
